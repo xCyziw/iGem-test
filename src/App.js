e4a2974914957a2643d2568607ffe53a25a2ee73
xCyziw <neonlight003@gmail.com>
2019-02-12 20:24:30 -0800

    complete fetch & display posts

--- src/App.js
+++ src/App.js
@@ -2,27 +2,73 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 class App extends Component {
+  state = {
+    isLoading: true,
+    posts: [],
+    error: null
+  }
+
+  fetchPosts() {
+    fetch(`https://jsonplaceholder.typicode.com/posts`)
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          posts: data,
+          isLoading: false,
+          query: '' // search query
+        })
+      )
+      .catch(error => this.setState({ error, isLoading: false }));
+  }
+
+  componentDidMount() {
+    this.fetchPosts();
+  }
+
+  handleInputChange = () => {
+    // to do
+  }
+
   render() {
+    const {isLoading, posts, error} = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div class="content">
+      <h1>Some Posts</h1>
+      {error ? <p>{error.message}</p> : null}
+
+      <form>
+        <input
+          placeholder="Search by post id..."
+          onChange={this.handleInputChange}
+        />
+        <input
+          placeholder="Search by user id..."
+          onChange={this.handleInputChange}
+        />
+      </form>
+
+      {!isLoading ? (
+        posts.map(post => {
+          const { id, userId, title, body } = post;
+          return (
+            <div key={id} class="post">
+            <p>Post #{id}, by User {userId}:</p>
+              <p class="post-title">{title}</p>
+              <p>{body}</p>
+            </div>
+          );
+        })
+      ) : (
+        <p>Loading...</p>
+      )}
       </div>
     );
   }
 }
 
 export default App;
+
+
